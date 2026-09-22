c778dfa0679f4aac3d6ab5a1e574a52a63a870de
JOY! <joydebnath747@gmail.com>
2026-09-22 12:07:43 +0600

    Update config.js

--- config.js
+++ config.js
@@ -33,9 +33,9 @@ class EnterpriseConfigurationManager {
              * If LAVALINK_HOST is not set, the public fallback nodes below are used.
              */
             lavalink: {
-                host: EnvironmentVariableProcessor.LAVALINK_HOST || "89.106.84.47",
-                port: EnvironmentVariableProcessor.LAVALINK_PORT || 2555,    
-                password: EnvironmentVariableProcessor.LAVALINK_PASSWORD || "https://discord.gg/archost",
+                host: EnvironmentVariableProcessor.LAVALINK_HOST || "lavalink.nazha.online",
+                port: EnvironmentVariableProcessor.LAVALINK_PORT || 443,    
+                password: EnvironmentVariableProcessor.LAVALINK_PASSWORD || "nazhafreelava",
                 secure: EnvironmentVariableProcessor.LAVALINK_SECURE === 'true' || false
 
                 /**
